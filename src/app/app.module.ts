b91716cce617fc039ea5c9d6120f3a9a7cb41224
rajat <rajat29gupta@gmail.com>
2018-11-06 13:24:12 +0530

    added customer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomersModule } from './customers/customers.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
