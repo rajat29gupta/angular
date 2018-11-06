c03d0b373f1259ac3d4d2ab15a1f8f10dbe62da8
rajat <rajat29gupta@gmail.com>
2018-11-06 14:04:18 +0530

    added filter component

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomersListComponent } from './customers-list/customers-list.component';
-
+import { FilterTextboxComponent } from './customers-list/filter-textbox.componetnt';
 @NgModule({
-  declarations: [CustomersListComponent],
+  declarations: [CustomersListComponent,FilterTextboxComponent],
   imports: [
     CommonModule
   ],
