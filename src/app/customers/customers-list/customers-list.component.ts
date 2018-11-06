368d99fae327de28066ff0bb4e99c41df6a2d8a7
rajat29gupta <rajat29gupta@gmail.com>
2018-11-06 17:22:49 +0530

    import data from parent componet
    using getter and setter using input property

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NullTemplateVisitor } from '@angular/compiler';
 import { findReadVarNames } from '@angular/compiler/src/output/output_ast';
 import { from } from 'rxjs';
@@ -10,7 +10,17 @@ import { ICustomer } from '../../shared/interfaces';
   styleUrls: ['./customers-list.component.css']
 })
 export class CustomersListComponent implements OnInit {
+  private _customers: ICustomer[] = [];
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
 
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
   filteredCustomers: any[] = [];
 
   customersOrderTotal: number;
@@ -19,12 +29,10 @@ export class CustomersListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.filteredCustomers=[
-      { id: 1, name: 'john Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
-      { id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22) },
-      { id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31) },
-      { id: 4, name: 'Jim Thomas', city: 'New York', orderTotal: 599.99, customerSince: new Date(2002, 10, 31) },
-    ];
+
+  }
+  sort(prop: string) {
+    //A sorter service will handle the sorting
   }
   calculateOrders() {
     this.customersOrderTotal = 0;
