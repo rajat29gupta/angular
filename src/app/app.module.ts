a228ce93627c21281b9a7e8c9a7b599b362b4fcc
rajat29gupta <rajat29gupta@gmail.com>
2018-11-06 22:10:43 +0530

    service and Http
    service Overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersComponent } from './customers/customers.component';
@@ -16,6 +18,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
     CustomersModule, SharedModule
   ],
   providers: [],
