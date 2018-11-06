3a44aef1a3658c65c13c503921c4838696bb385a
rajat29gupta <rajat29gupta@gmail.com>
2018-11-06 17:41:28 +0530

    added custom pipe
    added custom pipe to sort the data, currency and to export to the whole project

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.componetnt';
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [CustomersListComponent,FilterTextboxComponent],
   imports: [
-    CommonModule
+    CommonModule,SharedModule
   ],
   exports: [CustomersListComponent]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CapitalizePipe } from './capitalize.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [CapitalizePipe],
   imports: [
     CommonModule
   ],
-  exports:[]
+  exports: [CapitalizePipe]
 })
 export class SharedModule { }
